57a24600cb8b36001f17e6f3a7f00bfd0a7e569c
Mohd Faraz <mohdfarazharmis@gmail.com>
2025-10-13 16:19:14 +0530

    Create Capitalize.py

---
+++ Python/Capitalize.py
@@ -0,0 +1,34 @@
+'''
+You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
+Given a full name, your task is to capitalize the name appropriately.
+mohd faraz -> Mohd Faraz
+Input Format
+
+A single line of input containing the full name, S.
+
+Constraints
+0 < len(s) < 1000
+The string consists of alphanumeric characters and spaces.
+Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+
+Output Format
+Print the capitalized string, S.
+
+Sample Input
+chris alan
+Sample Output
+Chris Alan
+'''
+
+
+
+# Complete the solve function below.
+def solve(s):
+    s.capitalize()
+    out=""
+    for x in range(len(s)):
+        if s[x-1]==" " or x==0:
+            out=out+s[x].upper()
+        else:
+            out = out + s[x]
+    return (out)
